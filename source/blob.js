2229d80b452428297f2c50b3c72afef03d28dca1
Andrew Hoyer <me@andrew-hoyer.com>
2012-12-23 17:06:51 -0800

    Adding spline smoothing to the convex hull.
    After a ton of research I finally through together some spline calculations for the hull of the blob. I'm not entirely happy with this implementation as it falls apart when two points of the hull are close together. I've already tried a couple of times to fix the jankiness with no success. I guess I'll just leave it as is for now. Maybe it will grow on me.

--- source/blob.js
+++ source/blob.js
@@ -1,4 +1,5 @@
 var Blob = function (options){
+    this.color_ = options.color;
     this.world_ = options.world;
 
     // Bind this function early to help with adding and removing listeners.
@@ -12,97 +13,28 @@ var Blob = function (options){
     this.generateConstraints_();
 
     this.initializeEvents_();
+
+    this.time_ = 0;
 };
 
 Blob.prototype = {
     draw: function(context){
-        this.drawHull_(context);
+        var points = this.scleraPoints_.map(function(point){
+            return this.world_.toPixelsVec(point.current);
+        }, this);
+
+        context.strokeStyle = 'rgba(0, 0, 0, 0.3)';
+        context.fillStyle = this.color_;
+        Spline.draw(context, points, Blob.ROUNDING, Blob.PADDING);
+
+        this.world_.draw(context);
 
         for(var i = 0; i < Blob.MAX_EYES; i++)
             this.eyes_[i].draw(context);
     },
 
-    getConvexHull_: function(points){
-        var i, l;
-
-        // Compute the cross product between OA and OB.
-        function cross(o, a, b){
-            return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0]);
-        }
-
-        // Sort the points horizontally then vertically.
-        points.sort(function(p1, p2){
-            return (p1[0] - p2[0]) || (p1[1] - p2[1]);
-        });
-
-        var center = vec2.create();
-        var lower = [];
-        var upper = [];
-
-        for (i = 0, l = points.length; i < l; i++){
-            // Compute the lower hull.
-            while (lower.length >= 2 &&
-                cross(lower[lower.length - 2], lower[lower.length - 1], points[i]) <= 0){
-                lower.pop();
-            }
-            lower.push(points[i]);
-
-            // Compute the upper hull.
-            while (upper.length >= 2 &&
-                cross(upper[upper.length - 2], upper[upper.length - 1], points[l - i - 1]) <= 0){
-                upper.pop();
-            }
-            upper.push(points[l - i - 1]);
-
-            // Add this point to the center vector.
-            vec2.add(points[i], center);
-        }
-
-        // Scale the center vector by number of points to find the actual center.
-        vec2.scale(center, 1 / points.length);
-
-        // Remove the duplicate points.
-        upper.pop();
-        lower.pop();
-
-        // Expand the hull outward.
-        var hull = lower.concat(upper);
-        var scale = Blob.PADDING;
-        var offset = vec2.subtract(vec2.scale(center, scale, vec2.create()), center);
-        var expandedHull = [];
-        for (i = 0, l = hull.length; i < l; i++){
-            // Scale the hull point then subtract the center offset so that at the end the expanded
-            // hull encloses the original hull.
-            expandedHull.push(vec2.subtract(vec2.scale(hull[i], scale, vec2.create()), offset,
-                vec2.create()));
-        }
-
-        return expandedHull;
-    },
-
-    drawHull_: function(context){
-        var vectors = this.scleraPoints_.map(function(point){
-            return point.current;
-        });
-        var hullPoints = this.getConvexHull_(vectors).map(function(point){
-            return this.world_.toPixelsVec(point);
-        }, this);
-
-        context.strokeStyle = 'rgba(0, 0, 0, 0.2)';
-        context.fillStyle = 'rgba(255, 255, 255, 0.1)';
-        context.lineWidth = 1;
-        context.beginPath();
-        context.moveTo(hullPoints[0][0], hullPoints[0][1]);
-        hullPoints.slice(1).forEach(function(point){
-            context.lineTo(point[0], point[1]);
-        }, this);
-        context.lineTo(hullPoints[0][0], hullPoints[0][1]);
-        context.fill();
-        context.stroke();
-    },
-
     createEye_: function(){
-        var buffer = 0.1;
+        var buffer = 0.4;
         var x = (Math.random() * this.world_.width * (1 - buffer)) +
             (this.world_.width * buffer / 2);
         var y = Math.random() * (this.world_.height * (1 - buffer)) +
@@ -114,7 +46,7 @@ Blob.prototype = {
             radius: Eye.SCLERA_RADIUS,
             mass: Eye.SCLERA_MASS,
             interactive: true,
-            dampening: 0.05
+            dampening: 0.03
         });
 
         this.scleraPoints_.push(scleraPoint);
@@ -241,5 +173,6 @@ Blob.prototype = {
     }
 };
 
-Blob.MAX_EYES = 10;
+Blob.MAX_EYES = 8 + Math.floor(Math.random() * 3);
 Blob.PADDING = 1.5;
+Blob.ROUNDING = 0.3;
